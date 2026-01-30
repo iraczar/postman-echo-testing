5dabbb9249de61d56e42f2296480edeb694f3296
Ira.czar <ira.czar.02@bk.ru>
2026-01-30 19:20:50 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Postman Echo Testing Project
+
+![Build Status](https://github.com/iraczar/postman-echo-testing/actions/workflows/gradle.yml/badge.svg)
+
+Homework for Netology course: Postman Echo API Testing.
---
+++ src/test/java/ru/netology/postman/PostmanEchoTest.java
@@ -0,0 +1,25 @@
+package ru.netology.postman;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldReturnSentData() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Hello from Netology!")
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Hello from Netology!"));
+    }
+}
