f2d2ff0cd96bbee51c082c0d1da0e39d91d297d5
Ira.czar <ira.czar.02@bk.ru>
2026-01-30 19:25:28 +0300

    commit error

--- src/test/java/ru/netology/postman/PostmanEchoTest.java
+++ src/test/java/ru/netology/postman/PostmanEchoTest.java
@@ -20,6 +20,6 @@ public class PostmanEchoTest {
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Hello from Netology!"));
+                .body("data", equalTo("Wrong text"));
     }
 }
